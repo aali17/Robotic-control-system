9dd56f2e6e85dcaa88a8ff1ee66378912a4923af
aali17 <43824807+aali17@users.noreply.github.com>
2019-11-20 19:58:08 -0700

    Add files via upload
    Aircraft pitch control

---
+++ xInitZeroPD/xIinitZeroPD.m
@@ -0,0 +1,28 @@
+clc
+clear
+close all
+
+% Simulate the autopilot %
+options = odeset('RelTol',1e-4,'AbsTol',[1e-5 1e-5 1e-5]);
+tspan = linspace(0,20,5000);
+
+% Calling sequence: ode45(System function, time length, ICs, options)
+[T,x] = ode45(@AircraftPitchSystem,tspan,[0 0 0]',options);
+figure
+plot(T,x(:,3),'linewidth',2)
+xlabel('Time [sec.]','fontsize',16,'interpreter','latex')
+ylabel('Pitch $\theta(t)$ with r(t) = 0.2 rad.',...
+'fontsize',16,'interpreter','latex')
+title('PD controller')
+
+%% What exactly does the control u(t) look like? %
+for i = 1:length(T)
+    [dx,tu] = AircraftPitchSystem(T(i),x(i,:));
+    uk(i) = tu;
+end
+
+figure
+plot(T,uk,'linewidth',2)
+xlabel('Time [sec.]','fontsize',16,'interpreter','latex')
+ylabel('Control $u(t)$','fontsize',16,'interpreter','latex')
+
---
+++ xInitZeroPI_Actuator_limit/AircraftPitchSystem.m
@@ -0,0 +1,57 @@
+function [dx,ut] = AircraftPitchSystem(t,x)
+% Model of the aircraft system and Control gains %
+kp = 5; % Proportional control gain %
+kd = 1000; % Dirivative control gain (recall this has ? t wrapped in) %
+ki = 0.001; % Integral control gain (recall this has ? t wrapped in) %
+r = 0.2; % Desired pitch angle %
+e = r - x(3); % Pitch error %
+
+% Account for actuator saturation %
+u_limit = pi/4;
+
+% PID regulation - need to keep a running total of the error
+% as well as the old error for the derivative term %
+% ek is the total error accumulated, e_old is the previous error
+persistent ek e_old
+edot = kd*(e-e_old);
+eint = ki*sum(ek);
+epro = kp*e;
+
+% First time through e_old will be empty because there's no
+% intitialization (need to account for that)
+if(isempty(e_old))
+   u = 0;
+else
+   u = eint + epro;
+end
+
+% Check and limit actuators here %
+if u >= u_limit 
+    u = u_limit;
+elseif u <= -u_limit
+    u = -u_limit;
+end
+
+% Keep old values for control %
+e_old = e;
+ek = [ek;e];
+
+% Enter system dynamics here %
+% with no control input
+% dx = zeros(3,1);
+% dx(1) = -0.313*x(1) + 56.7*x(2);
+% dx(2) = -0.0139*x(1) - 0.426*x(2);
+% dx(3) = 56.7*x(2);
+
+% with control input
+dx = zeros(3,1);
+dx(1) = -0.313*x(1) + 56.7*x(2) + 0.232*u;
+dx(2) = -0.0139*x(1) - 0.426*x(2) + 0.0203*u;
+dx(3) = 56.7*x(2);
+
+% Need this to get the control signal out later %
+if nargout>1
+   ut = u;
+end
+ 
+ 
